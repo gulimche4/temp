68b68e98b769e586efebfdd04d501f2173e511df
gulimche <gulimche4@gmail.com>
2025-05-17 13:14:43 +0900

    띄어쓰기 수정

--- data/key.glsl
+++ data/key.glsl
@@ -3,5 +3,5 @@ vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords) 
     if (pixel.g == 1.0 && pixel.r == 0.0 && pixel.b == 0.0) {
         return vec4(0.0, 0.0, 0.0, 0.0);
     }
-    return pixel * color;
+    return pixel*color;
 }
