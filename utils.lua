802bae305937eee77f8a713e6915d5d72ba82adc
gulimchef <gulimche4@gmail.com>
2025-05-23 01:43:39 +0900

    유틸 수정

--- utils.lua
+++ utils.lua
@@ -28,9 +28,3 @@ end
 function rgba(r, g, b, a)
     return {r/255, g/255, b/255, a}
 end
-
--- https://lospec.com/palette-list/endesga-32
-COLOR_DARK = rgb(24, 20, 37)
-COLOR_LIGHT = rgb(255, 255, 255)
-
-TILE_SIZE = 16
